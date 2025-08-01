00b6559948ee5d6983a3f6cc40ac3dfca1380e3f
Haoran Zhao <1036066414@qq.com>
2025-08-01 19:44:05 +0800

    u

--- examples/league_animation.rs
+++ examples/league_animation.rs
@@ -1,11 +1,6 @@
-use bevy::{
-    // 引入新的 Mesh3d 和 MeshMaterial3d 组件
-    // 注意：根据 Bevy 版本，路径可能是 bevy::pbr 或 bevy::sprite
-    pbr::MeshMaterial3d,
-    prelude::*,
-};
+use bevy::{pbr::MeshMaterial3d, prelude::*};
 use binrw::BinRead;
-use moon_lol::render::LeagueSkeleton;
+use moon_lol::render::{AnimationFile, LeagueSkeleton};
 use std::fs::File;
 use std::io::BufReader;
 
@@ -21,18 +16,45 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    // --- 场景设置 (使用“必需组件”范式) ---
+    let path = "assets/sruap_orderturret1_idle1.anm";
+    println!("尝试解析文件: {}", path);
 
-    // 【1. 生成相机】
-    // 只需提供 Camera3d 组件和 Transform。
-    // 其他如 Projection, Frustum 等都会被自动添加。
+    let mut reader = BufReader::new(File::open(path).unwrap());
+    let asset = AnimationFile::read(&mut reader).unwrap();
+
+    // 使用 {:#?} 来进行格式化（pretty-print）输出
+    match asset {
+        AnimationFile::Compressed(modern) => {
+            println!("解析成功，版本: Compressed");
+            println!("{:#?}", modern.data.joint_hashes);
+        }
+        AnimationFile::Uncompressed(legacy) => {
+            println!("解析成功，版本: Uncompressed");
+            // println!("{:#?}", legacy.data);
+            match legacy.data {
+                moon_lol::render::UncompressedData::V3(uncompressed_data_v3) => {
+                    println!("V3");
+                }
+                moon_lol::render::UncompressedData::V4(uncompressed_data_v4) => {
+                    println!("V4");
+                }
+                moon_lol::render::UncompressedData::V5(uncompressed_data_v5) => {
+                    println!("V5{:#?}", uncompressed_data_v5.joint_hashes);
+                }
+            }
+        }
+    }
+
+    let animation_clip = AnimationClip::default();
+
+    animation_clip.add_curve_to_target(target_id, curve);
+    AnimatableCurve::new(property, curve)
+
     commands.spawn((
         Camera3d::default(),
         Transform::from_xyz(-250.0, 1000.0, 1000.0).looking_at(Vec3::new(0.0, 50.0, 0.0), Vec3::Y),
     ));
 
-    // 【2. 生成光源】
-    // 同理，只需提供 DirectionalLight 组件和 Transform。
     commands.spawn((
         DirectionalLight {
             shadows_enabled: true,
@@ -41,7 +63,6 @@ fn setup(
         Transform::from_rotation(Quat::from_euler(EulerRot::YXZ, -0.8, -0.5, 0.0)),
     ));
 
-    // --- 骨骼可视化 (使用“必需组件”范式) ---
     println!("正在解析骨骼文件: assets/turret.skl");
     let path = "assets/turret.skl";
     let skeleton = match File::open(path) {
@@ -62,8 +83,6 @@ fn setup(
     let mut joint_entities = vec![None; joints.len()];
 
     for (i, joint) in joints.iter().enumerate() {
-        // 【4. 生成每个关节小球】
-        // 同样使用最简洁的方式
         let current_entity = commands
             .spawn((
                 Mesh3d(sphere_mesh_handle.clone()),
